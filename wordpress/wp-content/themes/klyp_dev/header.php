02183a4936afd0035360f82870dc6085441ccc52
t10-ben <ben@talent10.com>
2021-03-07 22:02:27 +1000

    add title

--- wordpress/wp-content/themes/klyp_dev/header.php
+++ wordpress/wp-content/themes/klyp_dev/header.php
@@ -18,7 +18,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<link rel="profile" href="https://gmpg.org/xfn/11">
-		<title><?php wp_title(''); ?></title>
+		<title>Ben Fowler Klyp Developer Test | Round Two</title>
 		<?php wp_head(); ?>
 
 	</head>
